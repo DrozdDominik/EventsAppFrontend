9522794f15e377b5b4ffb394aef9d44b16dd01ff
Dominik Drozd <dominik_drozd@vp.pl>
2022-12-18 14:22:02 +0100

    Added logout button styles.

--- src/components/Navigation/LoggedNavigation.tsx
+++ src/components/Navigation/LoggedNavigation.tsx
@@ -1,48 +1,20 @@
 import React from 'react';
 import classes from './LoggedNavigation.module.css';
-import {useDispatch, useSelector} from "react-redux";
-import { authActions } from '../../store/auth-slice'
-import {apiUrl} from "../../config/api";
-import {NotificationStatus, uiAction} from "../../store/ui-slice";
+import { useSelector} from "react-redux";
 import {RootState} from "../../store";
 import { UserRole } from 'types'
 import {NavigateBtn} from "../common/Btns/NavigateBtn";
+import {LogoutBtn} from "../common/Btns/LogoutBtn";
 
 export const LoggedNavigation = () => {
     const { role } = useSelector((state: RootState) => state.auth )
-    const dispatch = useDispatch();
-
-    const logout = async () => {
-       const result = await fetch(`${apiUrl}/user/logout`, {method: "GET",
-            credentials: "include",
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-       })
-        if(result.status === 200) {
-            dispatch(authActions.logout())
-            dispatch(uiAction.showNotification({
-                status: NotificationStatus.info,
-                title: 'Poprawne wylogowanie',
-                message: 'Do zobaczenia!',
-                duration: 3500,
-            }))
-        } else {
-            dispatch((uiAction.showNotification({
-                status: NotificationStatus.error,
-                title: 'Błąd',
-                message: 'Nieudane wylogowanie!',
-            })))
-        }
-}
 
  return (
      <header>
          <nav>
              <ul className={classes.menuList}>
-                <li><button className={classes.logoutBtn} onClick={logout}>Wyloguj</button></li>
                  {role === UserRole.Editor && <li><NavigateBtn url={'/event/add'} text={'Dodaj wydarzenie'} /> </li>}
+                 <li><LogoutBtn /></li>
              </ul>
          </nav>
      </header>
