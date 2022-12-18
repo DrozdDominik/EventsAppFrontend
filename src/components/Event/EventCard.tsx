9ad0259a688491fb0da8bf9154d194da0a968eb3
Dominik Drozd <dominik_drozd@vp.pl>
2022-12-18 14:20:59 +0100

    Added event card styles.

--- src/components/Event/EventCard.tsx
+++ src/components/Event/EventCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MainEventData } from "types";
 import {NavigateBtn} from "../common/Btns/NavigateBtn";
+import classes from "./EventCard.module.css";
 
 interface Props {
     event: MainEventData
@@ -8,7 +9,7 @@ interface Props {
 
 export const EventCard = (props: Props) => {
     return (
-        <div>
+        <div className={classes.cardContainer}>
             <h3>{props.event.name}</h3>
             <p>{props.event.description}</p>
             <NavigateBtn url={`/event/${props.event.id}`} text={'Więcej informacji'}/>
