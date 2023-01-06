71936acbc7bf1ef2076ed469b0e4b360ebf71509
Dominik Drozd <dominik_drozd@vp.pl>
2023-01-06 22:18:35 +0100

    Added username retrieval and adjusted the displayed options for the editor for the UserPanel component.

--- src/pages/UserPanel/UserPanel.tsx
+++ src/pages/UserPanel/UserPanel.tsx
@@ -1,18 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './UserPanel.module.css';
 import { NavigateBtn } from '../../components/common/Btns/Navigate/NavigateBtn';
 import { DeleteBtn } from '../../components/common/Btns/Delete/DeleteBtn';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import { Spinner } from '../../components/Spinner/Spinner';
+import { apiUrl } from '../../config/api';
+import { UserRole } from 'types';
+
+const getUserName = async (): Promise<string> => {
+  const data = await fetch(`${apiUrl}/user/name`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const result: { name: string } = await data.json();
+
+  return result.name;
+};
 
 export const UserPanel = () => {
+  const { role } = useSelector((state: RootState) => state.auth);
+  const [userName, setUserName] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!role) {
+      return;
+    }
+
+    setLoading(true);
+
+    (async () => {
+      const name = await getUserName();
+      setUserName(name);
+      setLoading(false);
+    })();
+  }, []);
+
+  if (loading) {
+    return <Spinner isLoading={loading} />;
+  }
+
+  let permissions;
+
+  switch (role) {
+    case UserRole.User:
+      permissions = 'podstawowy';
+      break;
+    case UserRole.Editor:
+      permissions = 'edytor';
+      break;
+    case UserRole.Admin: {
+      permissions = 'admin';
+      break;
+    }
+  }
+
   return (
     <div className={classes.container}>
       <h2>Panel użytkownika</h2>
       <div className={classes.header}>
         <p>
-          Użytkownik: <span>User</span>
+          Użytkownik: <span>{userName}</span>
         </p>
         <p>
-          Poziom uprawnień: <span>podstawowy</span>
+          Poziom uprawnień: <span>{permissions}</span>
         </p>
       </div>
       <div>
@@ -26,9 +83,14 @@ export const UserPanel = () => {
           <li>
             <NavigateBtn url={'/user/password'} text={'Zmień hasło'} />
           </li>
-          <li>
-            <NavigateBtn url={'/user/role'} text={'Zwiększ poziom uprawnień'} />
-          </li>
+          {role === UserRole.User && (
+            <li>
+              <NavigateBtn
+                url={'/user/role'}
+                text={'Zwiększ poziom uprawnień'}
+              />
+            </li>
+          )}
           <li>
             <DeleteBtn />
           </li>
