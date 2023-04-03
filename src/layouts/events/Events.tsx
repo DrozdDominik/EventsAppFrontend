dcdd0cd5fc6daf1ecc680537ae3ac68ec8a72651
Dominik Drozd <dominik_drozd@vp.pl>
2023-04-03 14:13:24 +0200

    Refactored events layout style

--- src/layouts/events/Events.tsx
+++ src/layouts/events/Events.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { LoggedNavigation } from '../../components/Navigation/LoggedNavigation';
-import classes from './EventsLayout.module.css';
 
 export const EventsLayout = () => {
   return (
-    <div className={classes.layout}>
+    <div>
       <LoggedNavigation />
       <Outlet />
     </div>
