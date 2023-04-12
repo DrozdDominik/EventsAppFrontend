0da7310bddb17cc5148a562e9d0b2889dc26330c
Dominik Drozd <dominik_drozd@vp.pl>
2023-04-12 18:35:32 +0200

    Refactored single event loader function and component.

--- src/pages/Event/EventPage.tsx
+++ src/pages/Event/EventPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { EventDescription } from '../../components/EventDescription/EventDescription';
 import { EventEntity } from 'types';
 import {
@@ -7,12 +7,16 @@ import {
   redirect,
   useLoaderData,
   useNavigation,
+  useSearchParams,
 } from 'react-router-dom';
 import { MapDetailed } from '../../components/Map/MapDetailed';
-import { NavigateBtn } from '../../components/common/Btns/Navigate/NavigateBtn';
 import { Spinner } from '../../components/Spinner/Spinner';
 import classes from './EventPage.module.css';
 import { fetchGet } from '../../utils/fetch-get';
+import { getRole } from '../../utils/auth';
+import { NotificationStatus, uiAction } from '../../store/ui-slice';
+import { useDispatch } from 'react-redux';
+import { cleanUpLocalStorage } from '../../utils/clean-up-storage';
 
 type EventParams = {
   id: string;
@@ -20,48 +24,70 @@ type EventParams = {
 
 export const EventPage = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
   const isLoading = navigation.state === 'loading';
   const { event } = useLoaderData() as { event: EventEntity };
+  const permissions = searchParams.get('permissions');
+
+  useEffect(() => {
+    if (permissions === 'false') {
+      dispatch(
+        uiAction.showNotification({
+          status: NotificationStatus.info,
+          title: 'Brak uprawnień!',
+          message: '',
+          duration: 3500,
+        }),
+      );
+    }
+  }, []);
 
   if (isLoading) {
     return <Spinner isLoading={isLoading} />;
   }
 
   return (
-    <div className={classes.container}>
+    <>
       {event && (
-        <>
-          <div className={classes.main}>
-            <EventDescription event={event} />
-            <MapDetailed event={event} />
-          </div>
-          <div className={classes.navBtn}>
-            <NavigateBtn url={'/events'} text={'Wszystkie wydarzenia'} />
-          </div>
-        </>
+        <div className={classes.main}>
+          <EventDescription event={event} />
+          <MapDetailed event={event} />
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
 export const eventLoader: LoaderFunction = async ({ params }) => {
+  const role = getRole();
   const eventParams = params as unknown as EventParams;
+  const path = encodeURIComponent(`events/${eventParams.id}`);
+
+  if (!role) {
+    return redirect(`/?path=${path}&logged=false`);
+  }
+
   const data = await fetchGet(`api/event/${eventParams.id}`);
 
   if (!data.ok) {
     if (data.status === 401) {
-      return redirect('/auth');
+      cleanUpLocalStorage();
+      return redirect(`/?path=${path}&logged=false`);
     }
+
     if (data.status === 400 || data.status === 404) {
       throw json(
         { message: 'Szukane wydarzenie nie istnieje' },
         { status: 400 },
       );
     }
+
     throw json(
       { message: 'Błąd podczas pobierania wydarzenia' },
       { status: 500 },
     );
   }
+
   return data;
 };
