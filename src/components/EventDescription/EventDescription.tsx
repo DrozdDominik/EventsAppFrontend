ee3c8a5d2feb111e0cd660b260ef54627fd9557d
Dominik Drozd <dominik_drozd@vp.pl>
2023-05-06 12:21:24 +0200

    Added event start time and renamed estimatedTime to duration.

--- src/components/EventDescription/EventDescription.tsx
+++ src/components/EventDescription/EventDescription.tsx
@@ -15,12 +15,18 @@ export const EventDescription = (props: Props) => {
       </header>
       <h2 className={classes.title}>{props.event.name}</h2>
       <main className={classes.main}>
-        <p className={classes.time}>
-          Przewidywany czas trwania:{' '}
-          <span className={classes.minutes}>
-            {props.event.estimatedTime} minut
-          </span>
-        </p>
+        <div className={classes.duration}>
+          <p>
+            Godzina rozpoczęcia:{' '}
+            <span className={classes.time}>{props.event.time}</span>
+          </p>
+          <p>
+            Przewidywany czas trwania:{' '}
+            <span className={classes.minutes}>
+              {props.event.duration} minut
+            </span>
+          </p>
+        </div>
         <p className={classes.description}>{props.event.description}</p>
         {props.event.link && (
           <p className={classes.link}>
